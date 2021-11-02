571067e5b9e6ca437b83e456f0e1aded2f46b93a
silviotron <silvionovasmartinez@gmail.com>
2021-11-02 13:53:38 +0100

    primer ejercicio hecho

--- src/ejercicios05/Ejercicios05.java
+++ src/ejercicios05/Ejercicios05.java
@@ -5,6 +5,8 @@
  */
 package ejercicios05;
 
+import java.util.Scanner;
+
 /**
  *
  * @author silvi
@@ -19,17 +21,17 @@ public class Ejercicios05 {
         java.util.Scanner teclado = new java.util.Scanner(System.in);
         do{
             System.out.println("**************************************************");
-            System.out.println("*  1. todas las vocales                          *");
-            System.out.println("*  2. palindromo (string)                        *");
-            System.out.println("*  3. fecha valida                               *");
-            System.out.println("*  4. anagrama                                   *");
-            System.out.println("*  5. camelCase                                  *");
-            System.out.println("*  6. brakets                                    *");
+            System.out.println("*  1. Array aleatoria                            *");
+            System.out.println("*  2. ...                                        *");
+            System.out.println("*  3. ...                                        *");
+            System.out.println("*  4. ...                                        *");
+            System.out.println("*  5. ...                                        *");
+            System.out.println("*  6. ...                                        *");
             System.out.println("*  7. ...                                        *");
             System.out.println("*  8. ...                                        *");
-            System.out.println("*  9. (opcional) coderbyte 1º                    *");
-            System.out.println("* 10. (opcional) coderbyte 2º                    *");
-            System.out.println("* 11. (opcional) codificar                       *");
+            System.out.println("*  9. ...                                        *");
+            System.out.println("* 10. ...                                        *");
+            System.out.println("* 11. ...                                         *");
             System.out.println("*  0. Salir                                      *");
             System.out.println("**************************************************");
             entradaUsuario = teclado.nextLine();
@@ -77,7 +79,41 @@ public class Ejercicios05 {
     }
     
     public static void ejercicio01(){
+        Scanner sc = new Scanner(System.in);
+        int tamArray = -1;
+        do{
+            System.out.println("Por favor, inserte el tamaño del array");
+            if(sc.hasNextInt()){
+                tamArray = sc.nextInt();                
+            }
+            if(tamArray < 0){
+                System.out.println("ERROR: Debe insertar un número natural.\n");
+            }
+            sc.nextLine();
+        }
+        while(tamArray < 0);
+        int array[] = arrayAleatorio(tamArray);
+        for(int i = 0; i < array.length; i++){
+            System.out.println("posición " + i + " = " + array[i]);
+        }        
+        System.out.println("Suma total = " + sumArray(array));
+       
         
+    }
+    public static int[] arrayAleatorio(int tam){
+        int result[];
+        result = new int[tam];
+        for(int i = 0; i < result.length; i++){
+            result[i] = (int)(Math.random()*10);
+        }
+        return result;
+    }
+    public static int sumArray(int[] array){
+        int result = 0;
+        for(int i = 0; i < array.length; i++){
+            result = result + array[i];
+        }
+        return result;
     }
     
     public static void ejercicio02(){
